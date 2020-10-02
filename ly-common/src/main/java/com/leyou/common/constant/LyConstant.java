2fbe2584b9f397b4c5360dca0c197e2969ecb76e
= <=>
2020-10-02 21:31:37 +0800

    上传常量

---
+++ ly-common/src/main/java/com/leyou/common/constant/LyConstant.java
@@ -0,0 +1,12 @@
+package com.leyou.common.constant;
+
+public class LyConstant {
+    /**
+     * 品牌logo图片保存的文件夹
+     */
+    public static final String BRAND_IMG_FILE = "E:\\Projects\\leyou_work_develop\\nginx-1.16.0\\html\\brand-logo";
+    /**
+     * 品牌logo图片访问的url地址
+     */
+    public static final String BRAND_IMG_URL = "http://localhost/brand-logo/";
+}
