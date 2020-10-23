3781021d2b3df3c0eac38ab1c9ff7ae8c54514a4
= <=>
2020-10-23 22:23:33 +0800

    认证微服务创建

---
+++ ly-auth/src/main/java/com/leyou/LyAuthApplication.java
@@ -0,0 +1,20 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author zhongliang
+ * @date 2020/10/23 22:22
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class LyAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyAuthApplication.class,args);
+    }
+}
