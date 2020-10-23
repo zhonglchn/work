e08348d56681af38f809ceaf99b95917299773ee
= <=>
2020-10-23 13:39:34 +0800

    用户微服务

---
+++ ly-user/src/main/java/com/leyou/LyUserApplication.java
@@ -0,0 +1,19 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @author zhongliang
+ * @date 2020/10/23 13:30
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.leyou.user.mapper")
+public class LyUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LyUserApplication.class,args);
+    }
+}
