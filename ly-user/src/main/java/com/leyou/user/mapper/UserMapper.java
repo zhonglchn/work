8a19970ab5557eadfd3a0b95e8a87491c7f1a300
= <=>
2020-10-23 13:43:02 +0800

    UserMapper

---
+++ ly-user/src/main/java/com/leyou/user/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.leyou.user.mapper;
+
+import com.leyou.user.entity.User;
+import tk.mybatis.mapper.common.BaseMapper;
+
+/**
+ * @author zhongliang
+ * @date 2020/10/23 13:42
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
