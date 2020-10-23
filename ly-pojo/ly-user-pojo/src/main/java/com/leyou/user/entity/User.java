b17ad6c2401a76b4f3f3bb95a34c5418d015a458
= <=>
2020-10-23 13:41:58 +0800

    用户实体类

---
+++ ly-pojo/ly-user-pojo/src/main/java/com/leyou/user/entity/User.java
@@ -0,0 +1,21 @@
+package com.leyou.user.entity;
+
+import lombok.Data;
+import tk.mybatis.mapper.annotation.KeySql;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Table(name = "tb_user")
+@Data
+public class User {
+    @Id
+    @KeySql(useGeneratedKeys = true)
+    private Long id;
+    private String username;
+    private String password;
+    private String phone;
+    private Date createTime;
+    private Date updateTime;
+}
